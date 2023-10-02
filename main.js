668d6afa8762ce093e5c94f0f5ae46d30e37adad
Mr. Unknown <84503682+web-alt@users.noreply.github.com>
2023-10-02 11:04:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -10,26 +10,36 @@ function start()
  
 recognition.onresult = function(event) {
 
-     console.log(event); 
+ console.log(event); 
 
-    var Content = event.results[0][0].transcript;
+var Content = event.results[0][0].transcript;
 
     document.getElementById("textbox").innerHTML = Content;
     console.log(Content);
+      if(Content =="take my selfie")
+      {
+        console.log("taking selfie --- ");
         speak();
+      }
 }
 
 
 function speak(){
     var synth = window.speechSynthesis;
 
-    speak_data = document.getElementById("textbox").innerHTML;
+    speak_data = "Taking your Selfie in 5 seconds";
 
     var utterThis = new SpeechSynthesisUtterance(speak_data);
 
     synth.speak(utterThis);
 
     Webcam.attach(camera);
+
+    setTimeout(function()
+    { 
+        take_snapshot(); 
+        save();
+    }, 5000);
 }
 
  
@@ -40,4 +50,20 @@ Webcam.set({
     image_format : 'jpeg',
     jpeg_quality:90
 });
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+    });
+}
+
+
+function save()
+{
+  link = document.getElementById("link");
+  image = document.getElementById("selfie_image").src ;
+  link.href = image;
+  link.click();
+}
 
